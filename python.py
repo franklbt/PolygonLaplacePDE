c5794dc053b78f9055f4a3df20286e37c8411f2c
frank_lbt <frank.libolt@hotmail.com>
2016-05-06 07:23:01 +0200

    removed useless imports

--- python.py
+++ python.py
@@ -3,12 +3,8 @@
 
 import math
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
-import itertools
 
 n = 6 #number of edges
 N1 = 10
